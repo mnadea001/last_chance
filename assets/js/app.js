e7c46089c333dda5ed029f4c9ae8a1f959076f04
Mathilde Nadeau <mathilde.nadeau@hotmail.fr>
2022-02-22 15:04:10 +0100

    import font awesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,9 @@
  */
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
+require("@fortawesome/fontawesome-free/css/all.min.css");
+require("@fortawesome/fontawesome-free/js/all.js");
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
